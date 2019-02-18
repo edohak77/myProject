a28eef6b0c6cac05310fefbe79fd5bb8bdd07867
eduard <edohak77@gmail.com>
2019-02-18 15:12:43 +0400

    new commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -33,6 +33,7 @@ class ArticleController extends AbstractController
         ];
 
 
+
       return $this->render('article/show.html.twig',[
          'title'=> ucwords(str_replace('-',' ',$slug)),
           'comments'=>$comments,
